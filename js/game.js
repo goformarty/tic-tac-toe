c3b66a0c1fdc0981a5f4a2045840ee7cc6d1e4b3
GoForMarty <marty.stepien@gmail.com>
2017-05-08 00:25:39 +0100

    jquery added, unneccesary files deleted

--- js/game.js
+++ js/game.js
@@ -1,37 +1,184 @@
-// Constructs a game object to be played
-function Game() {
-
-	// initialise the new game with new state empty board configuration
-	this.newState = new State();
-	//"E" stands for empty board cell
-	this.newState.board = ["E", "E", "E",
-                  "E", "E", "E",
-                  "E", "E", "E"];
-
-    //X plays first
-	this.newState.turn = "X";
-	this.status = "beginning";
-
+function Position(board, turn) {
+    this.board = board || ["E","E","E","E","E","E","E","E","E"];
+    this.turn = turn || "X";
+    this.won = "";
 }
 
-// function that advances the game to a new state
-// @param _state [State]: the new state to advance the game to
-Game.prototype.advanceTo = function(_state) {
-	this.newState = _state;
-        if(_state.isTerminal()) {
-            this.status = "ended";
-        }
-        else {
-             //notify the AI player its turn has come up
-            this.ai.notify("O");
-            }
+Position.prototype.move = function(idx){
+    var position = new Position(this.board, this.turn === "X" ? "O" : "X");
+    position.board[idx] = this.turn;
+    return position;
 };
 
-// start game
-Game.prototype.start = function(){
-	if(this.status === "beginning") {
-            //invoke advanceTo with the initial state
-            this.advanceTo(this.newState);
-            this.status = "running";
-        }
+Position.prototype.possibleMoves = function(){
+    var indices = [];
+ for(var i = 0; i < 9 ; i++) {
+     if(this.board[i] === "E") {
+         indices.push(i);
+     }
+ }
+ return indices;
 };
+
+Position.prototype.isWinFor = function(turn) {
+    var B = this.board;
+
+        //check rows
+        for(var i = 0; i <= 6; i = i + 3) {
+         if(B[i] !== "E" && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
+                this.won = B[i];
+                return true;
+            }
+        }
+        //check columns
+        for(i = 0; i <= 2 ; i++) {
+         if(B[i] !== "E" && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
+                this.won = B[i];
+                return true;
+            }
+        }
+
+        //check diagonals
+        for( i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
+         if(B[i] !== "E" && B[i] == B[i + j] && B[i + j] === B[i + 2*j]) {
+                this.won = B[i];
+                return true;
+            }
+        }
+    return false;
+};
+
+Position.cache = {};
+
+Position.prototype.minimax = function(depth){
+
+    if(Position.cache[this.board]) { return Position.cache[this.board]}
+    
+    depth = depth || 0;
+
+    if (this.isWinFor() === true) {
+        if(this.won === "X"){
+            return 100 - depth;
+        }
+        else {
+            return -100 + depth; 
+        }
+    }
+
+    // to check if there are any empty squares
+    var pMoves = this.possibleMoves();
+    if(pMoves.length === 0) {
+        return 0;
+    }
+
+    var pNextMoves = pMoves.map(function(idx) {
+            return  position.move(idx).minimax(depth + 1);
+        });
+    if (this.turn === "X") {     
+        var largest = Math.max.apply(Math, pNextMoves);
+        Position.cache[this.board] = largest; 
+        return largest;
+
+    }
+    if (this.turn === "O") {    
+        var smallest  = Math.min.apply(Math, pNextMoves);
+        Position.cache[this.board] = smallest; 
+        return smallest;
+        
+    }
+};
+
+Position.prototype.bestMove = function() {
+    var bMove = Math.max.apply(Math, function(idx){
+        return position.move(idx).minimax();
+    });
+};
+
+
+
+// function State(oldState) {
+	
+//     //player who has the turn 
+//     this.turn = "";
+
+//     //number of moves of the AI player
+//     this.oMovesCount = 0;
+
+//     //result of the game in this State
+//     this.result = "still running";
+
+//     //board configuration in this state
+//     this.board = [];
+
+//     // if the state is constructed using a copy of old state
+//     if(typeof oldState !== "undefined") {
+//     	var oldLength = old.board.length;
+//     	this.board = new Array(oldLength);
+
+//     	for(var i = 0; i < oldLength; i++) {
+//     		this.board[i] = old.board[i];
+//     	}
+
+//     	this.oMovesCount = old.oMovesCount;
+//     	this.result = old.result;
+//     	this.turn = old.turn;
+//     }
+// }
+
+// State.prototype.advanceTurn = function() {
+// 	this.turn = this.turn === "X" ? "O" : "X";
+// 	return this.turn;
+// };
+
+// State.prototype.emptyCells = function() {
+// 	var indices = [];
+// 	for(var i = 0; i < 9 ; i++) {
+// 		if(this.board[i] === "E") {
+// 			indices.push(i);
+// 		}
+// 	}
+// 	return indices;
+// };
+
+// State.prototype.isTerminal = function() {
+// 	var B = this.board;
+
+//         //check rows
+//         for(var i = 0; i <= 6; i = i + 3) {
+//         	if(B[i] !== "E" && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
+//                 this.result = B[i] + "-won"; //update the state result
+//                 return true;
+//             }
+//         }
+
+//         //check columns
+//         for(i = 0; i <= 2 ; i++) {
+//         	if(B[i] !== "E" && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
+//                 this.result = B[i] + "-won"; //update the state result
+//                 return true;
+//             }
+//         }
+
+//         //check diagonals
+//         for( i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
+//         	if(B[i] !== "E" && B[i] == B[i + j] && B[i + j] === B[i + 2*j]) {
+//                 this.result = B[i] + "-won"; //update the state result
+//                 return true;
+//             }
+//         }
+
+//         var available = this.emptyCells();
+//         if(available.length === 0) {
+//             //the game is draw
+//             this.result = "draw"; //update the state result
+//             return true;
+//         }
+//         else {
+//         	return false;
+//         }
+//     };
+
+
+
+
+
--- test/spec/gameSpec.js
+++ test/spec/gameSpec.js
@@ -1,20 +1,118 @@
-describe("Game", function() {
-	var game;
+describe("Position", function() {
+	var position, position1;
+
 	beforeEach(function() {
-		game = new Game();
+		position = new Position();
 	});
+
 	it("creates a board and turn", function() {
-		expect(game.newState.board).toEqual(["E","E","E","E","E","E","E","E","E"]);
-		expect(game.newState.turn).toBe("X");
+		expect(position.board).toEqual(["E","E","E","E","E","E","E","E","E"]);
+		expect(position.turn).toBe("X");
 	});
-	it("advances turn to player O", function() {
-		expect(game.newState.advanceTurn()).toBe("O");
-	});
-	it("enumarates the empty cells and checks if the state is terminal", function() {
-		expect(game.newState.emptyCells()).toEqual([0,1,2,3,4,5,6,7,8]);
-		expect(game.newState.isTerminal()).toBe(false);
-		expect(game.newState.result).toBe("still running");
+	it ("takes two arguments", function(){
+		position = new Position(["X","E","E","E","E","E","E","E","E"], "O");
+		expect(position.board).toEqual(["X","E","E","E","E","E","E","E","E"]);
+		expect(position.turn).toBe("O");
 	});
 });
+
+describe("move", function(){
+	beforeEach(function() {
+		position = new Position();
+	});
+
+	it("updates board and turn", function(){
+		 position1 = new Position(["E","E","E","X","E","E","E","E","E"], "O");
+		expect(position.move(3)).toEqual(position1);
+	});
+});	
+
+describe("possible moves", function(){
+
+	beforeEach(function() {
+		position = new Position();
+	});
+
+	it("lists indices of empty squares", function(){
+		expect(position.move(3).move(5).possibleMoves()).toEqual([0,1,2,4,6,7,8]);
+	});
+});
+
+describe("isWinFor", function(){
+
+	beforeEach(function() {
+		position = new Position();
+	});
+
+	it("finds no win", function(){
+		expect(position.isWinFor("X")).toBe(false);
+	});
+	it("finds a win for X on the first row", function(){
+		position.board = ["X","X","X","E","E","E","E","E","E"];
+		expect(position.isWinFor("X")).toBe(true);
+	});
+	it("finds a win for X on the second row", function(){
+		position.board =["E","E","E","X","X","X","E","E","E"];
+		expect(position.isWinFor("X")).toBe(true);
+	});
+	it("finds a win for O on the second column", function(){
+		position.board =["E","O","E","E","O","E","E","O","E"];
+		expect(position.isWinFor("O")).toBe(true);
+	});
+	it("finds a win for O on the diagonal", function(){
+		position.board = ["O","E","E","E","O","E","E","E","O"];
+		expect(position.isWinFor("O")).toBe(true);
+	});
+});
+
+describe("minimax", function(){
+
+	beforeEach(function() {
+		position = new Position();
+	});
+
+	it("calculates a win for X", function(){
+		position.board = ["X","X","X","E","E","E","E","E","E"];
+		expect(position.minimax()).toEqual(100);
+	});
+	it("calculates a win for O", function(){
+		position.board = ["O","O","O","E","E","E","E","E","E"];
+		expect(position.minimax()).toEqual(-100);
+	});
+	it("calculates a draw", function(){
+		position.board = ["O","X","O","O","X","O","X","O","X"];
+		expect(position.minimax()).toEqual(0);
+	});
+	it("calculates a win for X in 1 move", function(){
+		position.board = ["X","X","E","E","E","E","E","E","E"];
+		expect(position.minimax()).toEqual(99);
+	});
+	it("calculates a win for O in 1 move", function(){
+		position.board = ["O","O","E","E","E","E","E","E","E"];
+		position.turn = "O";
+		expect(position.minimax()).toEqual(-99);
+	});
+});
+
+describe("best move", function(){
+
+	beforeEach(function() {
+		position = new Position();
+	});
+
+	it("finds the best move for x", function(){
+		position.board = ["X","X","E","E","E","E","E","E","E"];
+		expect(position.bestMove()).toEqual(0);
+	});
+});
+	// it("advances turn to player O", function() {
+	// 	expect(game.newState.advanceTurn()).toBe("O");
+	// });
+	// it("enumarates the empty cells and checks if the state is terminal", function() {
+	// 	expect(game.newState.emptyCells()).toEqual([0,1,2,3,4,5,6,7,8]);
+	// 	expect(game.newState.isTerminal()).toBe(false);
+	// 	expect(game.newState.result).toBe("still running");
+	// });
+
 
 
