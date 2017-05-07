7c945eb6d42b5c4f0561932eb0d527ef072bbd99
GoForMarty <marty.stepien@gmail.com>
2017-05-07 17:46:47 +0100

    terminal function update

--- js/game.js
+++ js/game.js
@@ -11,5 +11,27 @@ function Game() {
     //X plays first
 	this.newState.turn = "X";
 	this.status = "beginning";
+
 }
 
+// function that advances the game to a new state
+// @param _state [State]: the new state to advance the game to
+Game.prototype.advanceTo = function(_state) {
+	this.newState = _state;
+        if(_state.isTerminal()) {
+            this.status = "ended";
+        }
+        else {
+             //notify the AI player its turn has come up
+            this.ai.notify("O");
+            }
+};
+
+// start game
+Game.prototype.start = function(){
+	if(this.status === "beginning") {
+            //invoke advanceTo with the initial state
+            this.advanceTo(this.newState);
+            this.status = "running";
+        }
+};
