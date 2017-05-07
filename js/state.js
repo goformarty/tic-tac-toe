18d11ab0bea91e93c47d48f36709be71ffcb7ae9
GoForMarty <marty.stepien@gmail.com>
2017-05-07 09:44:02 +0100

    terminal function and tests added

--- js/state.js
+++ js/state.js
@@ -43,8 +43,42 @@ State.prototype.emptyCells = function() {
 };
 
 State.prototype.isTerminal = function() {
-	return false;
-};
+	var B = this.board;
+
+        //check rows
+        for(var i = 0; i <= 6; i = i + 3) {
+        	if(B[i] !== "E" && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
+                this.result = B[i] + "-won"; //update the state result
+                return true;
+            }
+        }
+
+        //check columns
+        for(i = 0; i <= 2 ; i++) {
+        	if(B[i] !== "E" && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
+                this.result = B[i] + "-won"; //update the state result
+                return true;
+            }
+        }
+
+        //check diagonals
+        for( i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
+        	if(B[i] !== "E" && B[i] == B[i + j] && B[i + j] === B[i + 2*j]) {
+                this.result = B[i] + "-won"; //update the state result
+                return true;
+            }
+        }
+
+        var available = this.emptyCells();
+        if(available.length === 0) {
+            //the game is draw
+            this.result = "draw"; //update the state result
+            return true;
+        }
+        else {
+        	return false;
+        }
+    };
 
 
 
--- test/spec/stateSpec.js
+++ test/spec/stateSpec.js
@@ -4,8 +4,45 @@ describe("State", function() {
 		state = new State();
 	});
 
-	it("advances the turn in a state", function() {
+	// checking if the state is terminal;
+	it("checks for rows", function() {
 		state.board = ["X", "X", "X", "O", "O", "E", "E", "E", "E"];
 		expect(state.isTerminal()).toBe(true);
+		expect(state.result).toBe("X-won");
+	});
+	it("checks for rows", function() {
+		state.board = ["X", "E", "X", "X", "E", "E", "O", "O", "O"];
+		expect(state.isTerminal()).toBe(true);
+		expect(state.result).toBe("O-won");
+	});
+	it("checks for columns", function() {
+		state.board = ["X", "E", "X", "X", "O", "O", "X", "E", "O"];
+		expect(state.isTerminal()).toBe(true);
+		expect(state.result).toBe("X-won");
+	});
+	it("checks for columns", function() {
+		state.board = ["X", "O", "E", "E", "O", "X", "X", "O", "X"];
+		expect(state.isTerminal()).toBe(true);
+		expect(state.result).toBe("O-won");
+	});
+	it("checks for diagonals", function() {
+		state.board = ["X", "O", "E", "E", "X", "E", "X", "O", "X"];
+		expect(state.isTerminal()).toBe(true);
+		expect(state.result).toBe("X-won");
+	});
+	it("checks for diagonals", function() {
+		state.board = ["X", "E", "O", "X", "O", "X", "O", "E", "X"];
+		expect(state.isTerminal()).toBe(true);
+		expect(state.result).toBe("O-won");
+	});
+	it("checks for a draw", function() {
+		state.board = ["X", "O", "O", "O", "X", "X", "X", "O", "O"];
+		expect(state.isTerminal()).toBe(true);
+		expect(state.result).toBe("draw");
+	});
+	it("expects the game to be still running", function() {
+		state.board = ["X", "O", "E", "E", "E", "E", "E", "E", "E"];
+		expect(state.isTerminal()).toBe(false);
+		expect(state.result).toBe("still running");
 	});
 });
